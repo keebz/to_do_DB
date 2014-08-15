9b72e51e01e7350a150810bd46f4ca1f72b62bb5
keebz <keebz@keebz.com>
2014-08-15 16:16:46 -0700

    creates CRUD functions for the list class and tests them.

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -5,4 +5,16 @@ describe List do
     create_test_objects
   end
 
+    it 'initializes a list class with a name.' do
+    expect(@test_list).to be_an_instance_of List
+  end
+
+  it 'will save the list to the lists database' do
+    expect(List.all).to eq [@test_list]
+  end
+
+  it 'should return name and list when asked.' do
+    expect(@test_list.name).to eq "school"
+  end
+
 end
